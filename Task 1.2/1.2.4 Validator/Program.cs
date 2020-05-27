0ba0e32a554b1bf4a55c1ac3b740cca5653130a1
AlexeyM196 <62460717+AlexeyM196@users.noreply.github.com>
2020-05-27 17:55:29 +0400

    1.2.4

---
+++ Task 1.2/1.2.4 Validator/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1._2._4_Validator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ЗАДАНИЕ НЕ ГОТОВО
+            //Console.WriteLine("Введите текст: ");
+            //string str = Console.ReadLine();
+            var base_string = "я плохо учил русский язык. забываю начинать предложения с заглавной. хорошо, что можно написать программу!";
+            var words = base_string.Replace(".", ".@").Replace("?", "?@").Replace("!", "!@").Split('@');
+            List<string> words1 = new List<string>();
+            
+            foreach (var word in words)
+            {
+                if (!string.IsNullOrEmpty(word) && char.IsLower(word[0]) && char.IsLetter(word[0]))
+                {
+                    words1.Add(word.Replace(word[0], char.ToUpper(word[0]))) ;
+                }
+            }
+            foreach(string word in words1)
+            {
+                Console.Write(word);
+            }
+        }
+    }
+}
